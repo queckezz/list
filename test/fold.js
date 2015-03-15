be7ab7a1223eba8917378cdc68d80f7bb13b7e79
queckezz <fabian.eichenberger@gmail.com>
2015-03-15 17:21:25 +0100

    test #fold() #foldr()

--- test/fold.js
+++ test/fold.js
@@ -1,10 +1,42 @@
-var foldl = require('../lib/fold')
+var fold = require('../lib/fold')
 var foldr = require('../lib/foldr')
 var Assert = require('assert')
 var Type = require('is')
 
 /*eslint-env mocha */
 
-describe('#foldl() #foldr()', function () {
-  // @todo
+describe('#fold()', function () {
+  var arr = [4, 6, 3]
+
+  it('should be curried', function () {
+    var acc = fold(0, function (acc, val) { return acc + val })
+    Assert.ok(Type.fn(acc))
+    Assert.equal(acc(arr), 13)
+  })
+
+  it('should fold left', function () {
+    var total = fold(0, function (acc, val) {
+      return acc + val
+    }, arr)
+
+    Assert.equal(total, 13)
+  })
+})
+
+describe('#foldr()', function () {
+  var arr = [2, 5, 20]
+
+  it('should be curried', function () {
+    var acc = foldr(0, function (acc, val) { return acc + val })
+    Assert.ok(Type.fn(acc))
+    Assert.equal(acc(arr), 27)
+  })
+
+  it('should fold right', function () {
+    var total = foldr(400, function (acc, val) {
+      return acc / val
+    }, arr)
+
+    Assert.equal(total, 2)
+  })
 })
